4b174e8613eda509f099f69168833ffdecd38b21
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-11-16 10:58:52 -0800

    tested locally

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,27 +10,37 @@ def get_employee_todo_progress(employee_id):
     if not isinstance(employee_id, int) or employee_id <= 0:
         raise ValueError('Employee id should be a positive integer')
 
-    url = f'https://jsonplaceholder.typicode.com/user/{employee_id}'
-    api_url_response = requests.get(url)
+    url_users = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    response_users = requests.get(url_users)
 
-    if api_url_response.status_code == 200:
-        data = api_url_response.json()
+    if response_users.status_code == 200:
+        employee_data = response_users.json()
+        employee_name = employee_data['name']
 
-        tasks_done = [todo for todo in data['todos'] if todo['completed']]
-        num_done = len(tasks_done)
-        num_total = len(data['todos'])
-        employee_name = data['name']
+        url_todos = f'https://jsonplaceholder.typicode.com/todos/'
+        f'?userId={employee_id}'
+        response_todos = requests.get(url_todos)
+        tasks_done = []
 
-        message = f'Employee {employee_name} is done with tasks\
-            ({num_done}/{num_total})'
-        print(message)
+        if response_todos.status_code == 200:
+            todos_data = response_todos.json()
+            tasks_done = [todo for todo in todos_data if todo['completed']]
+            num_done = len(tasks_done)
+            num_total = len(todos_data)
 
-        for task in tasks_done:
-            title = task['title']
-            print(f'\t {title}')
+            message = f'Employee {employee_name} is done with tasks'
+            f'({num_done}/{num_total})'
+            print(message)
+
+            for task in tasks_done:
+                title = task['title']
+                print(f'\t {title}')
+        else:
+            print(f'Error: Failed to retrieve information for employee'
+                  f' {employee_id}. Status code: {response_todos.status_code}')
     else:
-        print(f'Error: Failed to retrieve information.\
-              Status code: {api_url_response.status_code}')
+        print(f'Error: Failed to retrieve information for employee ID'
+              f' {employee_id}. Status code: {response_users.status_code}')
 
 
 if __name__ == '__main__':
